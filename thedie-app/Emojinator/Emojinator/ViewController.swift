5ac670287d8fa3b6da156fff4305d261ac618c6f
Lucas Cotta <lucas.oliveira.cotta@gmail.com>
2018-07-16 18:59:13 +0200

    Fixing UI with auto layout

---
+++ thedie-app/Emojinator/Emojinator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Emojinator
+//
+//  Created by Lucas Cotta on 7/16/18.
+//  Copyright © 2018 Lucas Cotta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func buttonPressed(_ sender: Any) {
+    }
+    
+}
+
