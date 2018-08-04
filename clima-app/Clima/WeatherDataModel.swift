2cd3ee9d04969c64e4da34c71820ee4941ee5fed
Lucas Cotta <lucas.oliveira.cotta@gmail.com>
2018-08-04 14:54:48 +0200

    updating location based on GPS, segues for input desired city

--- clima-app/Clima/WeatherDataModel.swift
+++ clima-app/Clima/WeatherDataModel.swift
@@ -3,58 +3,48 @@
 //  WeatherApp
 //
 //  Created by Angela Yu on 24/08/2015.
+//  Updated by Lucas Cotta
 //  Copyright (c) 2015 London App Brewery. All rights reserved.
-//
 
 import UIKit
 
 class WeatherDataModel {
 
-    //Declare your model variables here
-    
+    var city: String = ""
+    var temp: Int = 0
+    var condition: Int = 0
+    var humidity: Int = 0
+    var weatherIcon: String = ""
     
     //This method turns a condition code into the name of the weather condition image
     
-//    func updateWeatherIcon(condition: Int) -> String {
-//        
-//    switch (condition) {
-//    
-//        case 0...300 :
-//            return "tstorm1"
-//        
-//        case 301...500 :
-//            return "light_rain"
-//        
-//        case 501...600 :
-//            return "shower3"
-//        
-//        case 601...700 :
-//            return "snow4"
-//        
-//        case 701...771 :
-//            return "fog"
-//        
-//        case 772...799 :
-//            return "tstorm3"
-//        
-//        case 800 :
-//            return "sunny"
-//        
-//        case 801...804 :
-//            return "cloudy2"
-//        
-//        case 900...903, 905...1000  :
-//            return "tstorm3"
-//        
-//        case 903 :
-//            return "snow5"
-//        
-//        case 904 :
-//            return "sunny"
-//        
-//        default :
-//            return "dunno"
-//        }
-//
-//    }
+    func updateWeatherIcon(condition: Int) -> String {
+        
+    switch (condition) {
+        case 0...300 :
+            return "tstorm1"
+        case 301...500 :
+            return "light_rain"
+        case 501...600 :
+            return "shower3"
+        case 601...700 :
+            return "snow4"
+        case 701...771 :
+            return "fog"
+        case 772...799 :
+            return "tstorm3"
+        case 800 :
+            return "sunny"
+        case 801...804 :
+            return "cloudy2"
+        case 900...903, 905...1000  :
+            return "tstorm3"
+        case 903 :
+            return "snow5"
+        case 904 :
+            return "sunny"
+        default :
+            return "dunno"
+        }
+    }
 }
--- clima-app/Clima/WeatherViewController.swift
+++ clima-app/Clima/WeatherViewController.swift
@@ -14,12 +14,14 @@ import SwiftyJSON
 
 class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
-    //API constants
+    //API
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
     let APP_ID = "e0cbbc39346572a8ab122b0319679431"
     
     //GPS instatiation
     let locationManager = CLLocationManager()
+    //Data Model Instatiation
+    let weatherDataModel = WeatherDataModel()
     
     @IBOutlet weak var weatherIcon: UIImageView!
     @IBOutlet weak var cityLabel: UILabel!
@@ -34,7 +36,7 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
-        
+    
     }
     
     
@@ -44,40 +46,44 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
     //getWeatherData method
     func getWeatherData(url:String, weatherAPIParam: Dictionary<String, String>) {
-        Alamofire.request(url, method: .get, parameters: weatherAPIParam).responseJSON{
+        Alamofire.request(url, method: .get, parameters: weatherAPIParam).responseJSON {
             response in
             if response.result.isSuccess {
                 let WeatherDataInJSON: JSON = JSON(response.result.value!)
-                self.JSONParsing(JSONData: WeatherDataInJSON)
+                self.updateWeatherData(JSONData: WeatherDataInJSON)
             } else {
                 print("Error: \(String(describing: response.result.error))")
                 self.cityLabel.text = "Connection issues"
             }
         }
     }
-
     
-    
-    //MARK: - JSON Parsing
-    /***************************************************************/
-    func JSONParsing(JSONData: JSON) {
-        print(JSONData)
-        let humidity = JSONData["main"]["humidity"]
-        let temp = JSONData["main"]["temp"]
-        let location = JSONData["name"]
+    //updateWeatherData + JSON Parsing
+    func updateWeatherData(JSONData: JSON) {
+        
+        if let result = JSONData["main"]["temp"].double {
+            weatherDataModel.temp = Int(result - 273.15)
+            weatherDataModel.city = JSONData["name"].stringValue
+            weatherDataModel.condition = JSONData["weather"][0]["id"].intValue
+            weatherDataModel.humidity = JSONData["weather"][0]["id"].intValue
+            weatherDataModel.weatherIcon = weatherDataModel.updateWeatherIcon(condition: weatherDataModel.condition)
+            
+            updateUIWithWeatherData()
+        } else {
+            self.cityLabel.text = "Weather Unavailable"
+        }
     }
-    
-    //updateWeatherData method
-    
-
-    
-    
     
     //MARK: - UI Updates
     /***************************************************************/
     
     
     //updateUIWithWeatherData
+    func updateUIWithWeatherData(){
+        cityLabel.text = weatherDataModel.city
+        temperatureLabel.text = String(weatherDataModel.temp)
+        weatherIcon.image = UIImage(named: weatherDataModel.weatherIcon)
+    }
     
     
     
@@ -86,8 +92,6 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
     //MARK: - Location Manager Delegate Methods
     /***************************************************************/
-    
-    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let currentLocation = locations[locations.count - 1]
         if currentLocation.horizontalAccuracy > 0 {
